f7070d73d2ab2b24063a05669bcb5f2bb76bb6f9
spgurjar <s_patel@mt.iitr.ac.in>
2023-05-22 23:01:52 +0530

    AboutPage completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Main from './components/mainpage/mainpage';
 import Navbar from "./components/navbar/navbar.jsx"
+import About from "./components/AboutPage/About.jsx"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Navbar />
     <Main />
     <h1> Hello</h1>
+    <About/>
     </>
   )
 }
--- src/components/Health/Health.jsx
+++ src/components/Health/Health.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import honey from "./img/honey.png"
+import SugarcaneVinegar from "./img/vinegar.png"
+import Jaggery from "./img/Jaggery.jpg"
+import caneSugar from "./img/caneSugar.jpg"
 import ProductCard from "../Product/prcard";
 import "./health.css"
 
@@ -8,11 +11,10 @@ export default function Health(){
     return(
     <div className="card-container"> 
         <ProductCard name = "Honey" image = {honey} />
-        <ProductCard name = "Honey" image = {honey} />
-        <ProductCard name = "Honey" image = {honey} />
-        <ProductCard name = "Honey" image = {honey} />
-        <ProductCard name = "Honey" image = {honey} />
-        <ProductCard name = "Honey" image = {honey} />
+        <ProductCard name = "Sugarcane Vinegar" image = {SugarcaneVinegar} />
+        <ProductCard name = "Jaggery Powder" image = {Jaggery} />
+        <ProductCard name = "Cane Sugar" image = {caneSugar} />
+        
     </div>
     )
 }
